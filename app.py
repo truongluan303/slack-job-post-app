379cc1764020b7137f0c05c6df1272b9057ea90e
LuanTruong <truongluan303@gmail.com>
2022-05-30 15:41:55 -0700

    update

--- app.py
+++ app.py
@@ -1,31 +1,76 @@
+import base64
+import logging
+import math
+from functools import lru_cache
+from urllib import parse as urlparse
+
 from flask import Flask
 from slackeventsapi import SlackEventAdapter
 
-from config import SLACK_BOT_USER_OAUTH_TOKEN
 from config import SLACK_SIGNIN_SECRET
 
 
 app = Flask(__name__)
 
 
-print(SLACK_BOT_USER_OAUTH_TOKEN)
-print(SLACK_SIGNIN_SECRET)
+_LOGGER = logging.getLogger(__name__)
 
 
 slack_ev_adapter = SlackEventAdapter(SLACK_SIGNIN_SECRET, "/slack/events", app)
 
 
-# @slack_ev_adapter.on('message')
-# def message(payload):
-#     event = payload.get('event', {})
-#     text = event.get('text')
+@lru_cache(maxsize=60)
+def isPrime(i):
+    if i in [2, 3]:  # shortcut low primes
+        return True
+    else:
+        if i % 2 == 0 or i % 3 == 0:  # special since we go 3-> sqrt
+            return False
+        sqrt = int(math.sqrt(i) // 1)
+        for s in range(
+            3, sqrt + 1, 2
+        ):  # check odd vals, or all prior primes + new primes
+            if i % s == 0:
+                return False
+        return True
 
-#     if 'say_hello' in text.lower():
-#         msg = 'hello world!'
-#         MESSAGE_SCHEMA['text']['text'] = msg
-#         x = {'channel': JOB_POSTING_CHANNEL, 'blocks': [MESSAGE_SCHEMA]}
-#         return slack_wclient.chat_postMessage(**x)
+
+commands = {"isprime": isPrime, "prime": isPrime}  # map of command aliases
 
 
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port=8080)
+def lambda_handler(event, context):
+    # data comes b64 and also urlencoded name=value& pairs
+    msg_map = dict(
+        urlparse.parse_qsl(base64.b64decode(str(event["body"])).decode("ascii"))
+    )
+    command = msg_map.get("command", "err")  # will be /command name
+    params = msg_map.get("text", "err").split(" ")  # params ['isPrime','50']
+    subcommand = params[0].lower()
+
+    if len(params) < 2:
+        response = f"available subcommands: {list(commands.keys())} + 1 parameter"
+    elif subcommand in commands.keys():
+        response = (
+            f"{subcommand} needs an numeric param"
+            if len(params) < 2
+            else f"{subcommand} = {commands[subcommand](int(float(params[1])))}"
+        )
+    else:
+        response = f"illegal sub command >{subcommand}<, commands available {list(commands.keys())}"
+
+    # logging
+    print(
+        str(command)
+        + " "
+        + str(params)
+        + " -> "
+        + response
+        + ",original: "
+        + str(msg_map)
+    )
+
+    return {
+        "response_type": "in_channel",
+        "text": command + " " + " ".join(params),
+        "attachments": [{"text": response}],
+    }
